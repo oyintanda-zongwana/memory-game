01c59837d06be6b191765efc9aca346c698529bf
oyintanda <oyintandazongone@gmail.com>
2025-01-22 21:25:24 +0200

    made a navbar and made game functional :construction:

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/level')
+def level():
+    return render_template('game.html')
+
+@app.route('/nav')
+def nav():
+    return render_template('navbar.html')
+
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
